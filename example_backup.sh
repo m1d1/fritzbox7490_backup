b0a04b279316f06d28c5e06d8aeab3432159b02b
Michael Dinkelaker <michael.dinkelaker@gmail.com>
2016-06-03 16:25:21 +0200

    initial commit

---
+++ example_backup.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+##
+#  An example how to use lib_fritz7490
+#  adjust vars _FBOX, _PASSWORD, _DESTINATION 
+#  and the name of the default Phonebook in line 25
+#  to backup your FRITZ!Box settings and phonebook.
+##
+
+#  File:    example_backup.sh
+#  author:  Michael Dinkelaker,
+#           michael[dot]dinkelaker[at]gmail[dot]com
+#  date:    2016-06-03
+
+
+_FBOX="http://192.178.0.1"
+_PASSWORD="secret"
+_EXPORT_PASSWORD="same_or_another_secret"
+ROTATE_PERIOD=180
+DESTINATION="/rainbow/unicorn/FritzBox/"
+
+source ./lib_fritz7490.sh
+login
+export_settings $_EXPORT_PASSWORD >$DESTINATION/$(date +%Y-%m-%d)_fritzbox_settings.cfg
+export_phonebook 0 Phonebook >$DESTINATION/$(date +%Y-%m-%d)_fritzbox_phonebook.xml
+#export_phonebook 1 Work >$DESTINATION/$(date +%Y-%m-%d)_fritzbox_phonebook_work.xml
+
+# delete files older than $ROTATE_PERIOD days 
+find $DESTINATION -mtime +$ROTATE_PERIOD -exec rm {} \;
